3828af8ff391b55280be6459409f0520cf67156b
Arguello Rincon <CamiloAndres.ArguelloRincon@cencosud.com.co>
2018-12-06 17:30:26 -0500

    Ajustes de estilos y animaciones

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
 require("./index.scss");
-console.log("works")
+
+$(function () {    
+    setInterval(function () {
+        $('.star-1').fadeOut(300).delay(200).fadeIn(500).fadeOut(500).delay(554);
+        $('.star-2').fadeOut(300).fadeIn(400).fadeOut(1000).delay(920);
+        $('.star-3').fadeOut(150).delay(100).fadeIn(500).fadeOut(650).delay(100);
+        $('.star-4').fadeOut(700).fadeIn(300).fadeOut(500).delay(1350);
+        $('.star-5').fadeOut(200).fadeIn(500).fadeOut(1000).delay(100);
+        $('.star-6').fadeOut(150).delay(100).fadeIn(500).fadeOut(650).delay(100);
+        $('.star-7').fadeOut(300).fadeIn(400).fadeOut(1000).delay(920);
+
+        setTimeout(() => {
+            $(".light").css("visibility","visible");
+        }, 200);
+        
+        $('.light-1').fadeOut(300).delay(200).fadeIn(500).fadeOut(500).delay(554);
+        $('.light-2').fadeOut(300).fadeIn(400).fadeOut(1000).delay(920);
+        $('.light-3').fadeOut(150).delay(100).fadeIn(500).fadeOut(650).delay(100);
+        $('.light-4').fadeOut(700).fadeIn(300).fadeOut(500).delay(1350);
+        $('.light-5').fadeOut(200).fadeIn(500).fadeOut(1000).delay(100);
+        $('.light-6').fadeOut(150).delay(100).fadeIn(500).fadeOut(650).delay(100);
+        $('.light-7').fadeOut(300).fadeIn(400).fadeOut(1000).delay(920);
+        $('.light-8').fadeOut(300).delay(200).fadeIn(500).fadeOut(500).delay(554);
+    }, 1);
+    
+    new WOW().init();
+});
